3deef58633305582ad688b640b088ff09f0b3e9c
Evan Pratten <ewpratten@gmail.com>
2020-02-12 12:34:17 -0500

    Question 2

--- competition/Question2.py
+++ competition/Question2.py
@@ -1,6 +1,12 @@
 
-def findJumpsForVal(val: int) -> list:
-    pass
+def findJumpsForVal(grid: list, val: int) -> list:
+    # Find all xy combinations that multiply to val
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+
+            # Check if it is a factor
+            if (r+1)*(c+1) == val:
+                yield (r, c)
 
 
 # Read the number of lines
@@ -13,7 +19,7 @@ input("")
 grid: list = []
 while len(grid) < line_count:
     # Add row to the grid
-    grid.append([int(i) for i in input("").split(" ")])
+    grid.append([int(i) for i in input("").strip().split(" ")])
 
 # Define a tasks queue for jumps and tracker for visited points
 tasks: list = []
@@ -26,6 +32,7 @@ tasks.append({
     "value": grid[0][0]
 })
 
+
 # Handle tasks running
 while len(tasks) > 0:
 
@@ -33,24 +40,25 @@ while len(tasks) > 0:
     task: dict
     try:
         task = tasks.pop(0)
-    except ValueError as e:
+    except IndexError as e:
         break
 
     # Mark this as a visited position
     visited.append((task["x"], task["y"]))
 
     # Find all allowed jumps
-    jumps: list = findJumpsForVal(task["value"])
+    jumps: list = list(findJumpsForVal(grid, task["value"]))
 
     # Check all jumps
     for jump in jumps:
 
         # Jump not valid if we have been there before
+        # print(visited)
         if jump in visited:
             continue
 
         # If the jump is the end, we have finished
-        if jump == ():
+        if jump == (len(grid)-1, len(grid[0])-1):
             print("yes")
             exit(0)
 
@@ -58,7 +66,7 @@ while len(tasks) > 0:
         tasks.append({
             "x": jump[0],
             "y": jump[1],
-            "value": grid[jump[1]][jump[0]]
+            "value": grid[jump[0]][jump[1]]
         })
 
 
